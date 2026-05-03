973bea88a237c701482b7306b97c4a7763cb895a
lyu2026 <lyuw2026@gmail.com>
2026-05-03 20:23:59 +0800

    .

--- KoofrPlugin.java
+++ KoofrPlugin.java
@@ -1,12 +1,13 @@
 package com.koofr.cordova;
 
+import android.util.Base64;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import com.silkimen.http.HttpRequest;
 import java.net.URLEncoder;
 import org.json.JSONObject;
 import org.json.JSONArray;
-import android.util.Base64;
 
 public class KoofrPlugin extends CordovaPlugin{
 	private static final String K="https://app.koofr.net/content/api/v2";
@@ -14,46 +15,73 @@ public class KoofrPlugin extends CordovaPlugin{
 	private static final String U="lyuw2026@gmail.com";
 	private static final String P="vy75aqa4naicde1r";
 	private String M;
-	private volatile boolean R;
+	private volatile boolean I;
+	private final Object L=new Object();
 
 	@Override
 	protected void pluginInitialize(){
-		go(new Task(){public void run()throws Exception{
-			M=mid();
-			R=true;
-		}});
+		cordova.getThreadPool().execute(new Runnable(){
+			@Override
+			public void run(){
+				try{
+					M=new JSONObject(req("/mounts","GET",null,null)).getJSONArray("mounts").getJSONObject(0).getString("id");
+					synchronized(L){I=true;}
+				}catch(Exception e){}
+			}
+		});
 	}
 
 	@Override
 	public boolean execute(String a,JSONArray x,CallbackContext z){
-		if("up".equals(a)){run(z,()->up(x,z),()->!R);return true;}
-		if("dn".equals(a)){run(z,()->dn(x,z),()->!R);return true;}
-		if("rm".equals(a)){run(z,()->rm(x,z),()->!R);return true;}
-		if("ls".equals(a)){run(z,()->ls(x,z),()->!R);return true;}
+		if("up".equals(a)){go(z,()->up(x,z));return true;}
+		if("dn".equals(a)){go(z,()->dn(x,z));return true;}
+		if("rm".equals(a)){go(z,()->rm(x,z));return true;}
+		if("ls".equals(a)){go(z,()->ls(x,z));return true;}
 		return false;
 	}
 
-	private interface Task{void run()throws Exception;}
+	private interface T{void run()throws Exception;}
 
-	private void go(Task t){cordova.getThreadPool().execute(()->{try{t.run();}catch(Exception e){}});}
-
-	private void run(CallbackContext z,Task t,Check c){
-		cordova.getThreadPool().execute(()->{
-			try{
-				if(c.ok()){waitInit();}
-				t.run();
-			}catch(Exception e){z.error(e.getMessage());}
+	private void go(CallbackContext z,T t){
+		cordova.getThreadPool().execute(new Runnable(){
+			@Override
+			public void run(){
+				try{
+					waitInit();
+					t.run();
+				}catch(Exception e){err(z,e.getMessage());}
+			}
 		});
 	}
 
-	private interface Check{boolean ok();}
-
-	private synchronized void waitInit()throws Exception{
-		while(!R){try{wait(100);}catch(InterruptedException e){throw new Exception("初始化中断");}}
+	private void waitInit()throws Exception{
+		if(!I){
+			synchronized(L){
+				long t=10000;
+				while(!I&&t>0){
+					long s=System.currentTimeMillis();
+					try{L.wait(500);}catch(InterruptedException e){throw new Exception("初始化中断");}
+					t-=System.currentTimeMillis()-s;
+				}
+				if(!I)throw new Exception("插件初始化超时");
+			}
+		}
 	}
 
-	private String mid()throws Exception{
-		return new JSONObject(req("/mounts","GET",null,null)).getJSONArray("mounts").getJSONObject(0).getString("id");
+	private void err(CallbackContext z,String m){
+		if(z!=null&&!z.isFinished()){
+			PluginResult r=new PluginResult(PluginResult.Status.ERROR,m);
+			r.setKeepCallback(false);
+			z.sendPluginResult(r);
+		}
+	}
+
+	private void ok(CallbackContext z,String d){
+		if(z!=null&&!z.isFinished()){
+			PluginResult r=new PluginResult(PluginResult.Status.OK,d);
+			r.setKeepCallback(false);
+			z.sendPluginResult(r);
+		}
 	}
 
 	private String req(String p,String m,byte[] b,String t)throws Exception{
@@ -82,18 +110,18 @@ public class KoofrPlugin extends CordovaPlugin{
 	private void up(JSONArray x,CallbackContext z)throws Exception{
 		String p=x.optString(0,""),n=x.optString(1,"");
 		byte[] b=arr(x,2);
-		if(n.isEmpty()){z.error("文件名不能为空!");return;}
+		if(n.isEmpty()){err(z,"文件名不能为空!");return;}
 		p=fp(p);
 		String o=req("/mounts/"+M+"/files/put?path="+URLEncoder.encode(p,"UTF-8")+"&filename="+URLEncoder.encode(n,"UTF-8")+"&info=true&overwrite=true","POST",b,"application/octet-stream");
-		z.success(new JSONObject(o));
+		ok(z,o);
 	}
 
 	private void dn(JSONArray x,CallbackContext z)throws Exception{
 		String p=fc(x.optString(0,"")),n=x.optString(1,"");
-		if(n.isEmpty()){z.error("文件名不能为空!");return;}
+		if(n.isEmpty()){err(z,"文件名不能为空!");return;}
 		p=fp(p);
 		String o=req("/mounts/"+M+"/files/get/"+URLEncoder.encode(n,"UTF-8")+"?path="+URLEncoder.encode(p+"/"+n,"UTF-8")+"&force=true","GET",null,null);
-		z.success(o);
+		ok(z,o);
 	}
 
 	private void rm(JSONArray x,CallbackContext z)throws Exception{
@@ -108,11 +136,11 @@ public class KoofrPlugin extends CordovaPlugin{
 			o.put(f);
 		}
 		JSONObject r=new JSONObject();r.put("files",o);
-		z.success(new JSONObject(req("/jobs/files/remove","POST",r.toString().getBytes(),"application/json")));
+		ok(z,req("/jobs/files/remove","POST",r.toString().getBytes(),"application/json"));
 	}
 
 	private void ls(JSONArray x,CallbackContext z)throws Exception{
 		String p=fp(fc(x.optString(0,"")));
-		z.success(new JSONObject(req("/mounts/"+M+"/files/list?path="+URLEncoder.encode(p,"UTF-8"),"GET",null,null)));
+		ok(z,req("/mounts/"+M+"/files/list?path="+URLEncoder.encode(p,"UTF-8"),"GET",null,null));
 	}
 }
